cf76bf20c8432fc335c9687bbe622d1663e591f0
Matt DeKrey <matt.dekrey@principlestudios.com>
2022-03-04 13:19:32 -0600

    Initial next.js commit

---
+++ PrincipleStudios.PlanningPoker.FrontEnd/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Planning Poker presented by Principle Studios</title>
+        <meta name="description" content="Principle Studios presents Planning Poker per Project Pointing" />
+      </Head>
+
+      Hello World
+    </div>
+  );
+}
